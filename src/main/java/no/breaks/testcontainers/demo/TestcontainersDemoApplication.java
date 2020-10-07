acd1dac212556a53a17cb1bd46c825133b5f01c9
Marius Breivik <marius.breivik@gmail.com>
2020-10-07 23:42:17 +0200

    Initial commit

---
+++ src/main/java/no/breaks/testcontainers/demo/TestcontainersDemoApplication.java
@@ -0,0 +1,13 @@
+package no.breaks.testcontainers.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestcontainersDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestcontainersDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/breaks/testcontainers/demo/TestcontainersDemoApplicationTests.java
@@ -0,0 +1,47 @@
+package no.breaks.testcontainers.demo;
+
+import com.sun.istack.NotNull;
+import org.junit.After;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.testcontainers.containers.JdbcDatabaseContainer;
+import org.testcontainers.containers.MSSQLServerContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ContextConfiguration(initializers = TestcontainersDemoApplicationTests.Initializer.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@SpringBootTest
+@Testcontainers
+class TestcontainersDemoApplicationTests {
+
+	@Container
+	private static JdbcDatabaseContainer mssqlServerContainer = new MSSQLServerContainer("mcr.microsoft.com/mssql/server:2019-latest")
+			.acceptLicense();
+
+	public static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+		@Override
+		public void initialize(@NotNull ConfigurableApplicationContext configurableApplicationContext) {
+			configurableApplicationContext.getEnvironment().getSystemProperties().put("spring.datasource.url", mssqlServerContainer.getJdbcUrl() );
+			configurableApplicationContext.getEnvironment().getSystemProperties().put("spring.datasource.username", mssqlServerContainer.getUsername());
+			configurableApplicationContext.getEnvironment().getSystemProperties().put("spring.datasource.password", mssqlServerContainer.getPassword());
+			mssqlServerContainer.withConnectTimeoutSeconds(24);
+			mssqlServerContainer.start();
+
+		}
+	}
+
+	@Test
+	void contextLoads() {
+		mssqlServerContainer.start();
+		assertTrue(true);
+	}
+
+}
